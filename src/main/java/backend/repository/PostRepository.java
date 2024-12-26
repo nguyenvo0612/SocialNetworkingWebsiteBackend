29a8a3e4885980d3020f960284e5237ee7090328
nguyenvo0612 <ngvo.1203@gmail.com>
2024-12-26 21:30:38 +0700

    update

--- src/main/java/backend/repository/PostRepository.java
+++ src/main/java/backend/repository/PostRepository.java
@@ -1,4 +1,9 @@
 package backend.repository;
 
-public interface PostRepository {
+import backend.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Integer> {
 }
