b0eaabfea3184b3d2e982fbceac79d994f97d58d
nguyenvo0612 <ngvo.1203@gmail.com>
2024-12-28 16:27:55 +0700

    update

--- src/main/java/backend/repository/PostImageRepository.java
+++ src/main/java/backend/repository/PostImageRepository.java
@@ -2,6 +2,9 @@ package backend.repository;
 
 import backend.entity.PostImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostImageRepository extends JpaRepository<PostImage, Long> {
+
 }
--- src/main/java/backend/service/PostService.java
+++ src/main/java/backend/service/PostService.java
@@ -1,4 +1,18 @@
 package backend.service;
 
+import backend.entity.Post;
+import backend.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PostService {
+    @Autowired
+    private PostRepository postRepository;
+
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
 }
