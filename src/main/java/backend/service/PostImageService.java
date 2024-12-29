47ef3a270ef4733ca1ee53777a4e508acbe6271c
nguyenvo0612 <ngvo.1203@gmail.com>
2024-12-29 22:17:07 +0700

    update

--- src/main/java/backend/service/PostImageService.java
+++ src/main/java/backend/service/PostImageService.java
@@ -1,4 +1,18 @@
 package backend.service;
 
+import backend.entity.PostImage;
+import backend.repository.PostImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PostImageService {
+    @Autowired
+    private PostImageRepository postImageRepository;
+
+    public List<PostImage> getAllPostImages() {
+        return postImageRepository.findAll();
+    }
 }
