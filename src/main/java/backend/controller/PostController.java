faace4c3f8ece97de9ea087221f9219f641e049a
nguyenvo0612 <ngvo.1203@gmail.com>
2024-12-30 16:37:30 +0700

    update

--- src/main/java/backend/controller/PostController.java
+++ src/main/java/backend/controller/PostController.java
@@ -1,4 +1,22 @@
 package backend.controller;
 
+import backend.entity.Post;
+import backend.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/post")
 public class PostController {
+    @Autowired
+    private PostService postService;
+
+    @GetMapping("/getAll")
+    public List<Post> getAll() {
+        return postService.getAllPosts();
+    }
 }
