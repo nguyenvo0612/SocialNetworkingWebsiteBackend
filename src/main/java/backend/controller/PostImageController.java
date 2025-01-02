99a20c0a946dde483f66b2b03b0778796dfef944
nguyenvo0612 <ngvo.1203@gmail.com>
2025-01-02 23:03:38 +0700

    update

--- src/main/java/backend/controller/PostImageController.java
+++ src/main/java/backend/controller/PostImageController.java
@@ -1,4 +1,22 @@
 package backend.controller;
 
+import backend.entity.PostImage;
+import backend.service.PostImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/post-image")
 public class PostImageController {
+    @Autowired
+    private PostImageService postImageService;
+
+    @GetMapping
+    public List<PostImage> getPostImages() {
+        return postImageService.getAllPostImages();
+    }
 }
